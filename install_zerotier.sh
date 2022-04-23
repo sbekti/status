4feadfb73893c48eaec6986d0e5de803031528ed
Samudra Harapan Bekti <samudra.bekti@gmail.com>
2022-04-23 12:15:53 -0400

    Force restart after ID override

--- install_zerotier.sh
+++ install_zerotier.sh
@@ -304,32 +304,16 @@ if [ ! -e /usr/sbin/zerotier-one ]; then
 fi
 
 echo
-echo '*** Setting ZeroTier identity...'
-
-sudo mkdir -p /var/lib/zerotier-one
-echo $ZT_IDENTITY_PUBLIC | sudo tee /var/lib/zerotier-one/identity.public > /dev/null
-echo $ZT_IDENTITY_SECRET | sudo tee /var/lib/zerotier-one/identity.secret > /dev/null
-
-echo
 echo '*** Enabling and starting ZeroTier service...'
 
-if [ -e /usr/bin/systemctl -o -e /usr/sbin/systemctl -o -e /sbin/systemctl -o -e /bin/systemctl ]; then
-	$SUDO systemctl enable zerotier-one
-	$SUDO systemctl start zerotier-one
-	if [ "$?" != "0" ]; then
-		echo
-		echo '*** Package installed but cannot start service! You may be in a Docker'
-		echo '*** container or using a non-standard init service.'
-		echo
-		exit 1
-	fi
-else
-	if [ -e /sbin/update-rc.d -o -e /usr/sbin/update-rc.d -o -e /bin/update-rc.d -o -e /usr/bin/update-rc.d ]; then
-		$SUDO update-rc.d zerotier-one defaults
-	else
-		$SUDO chkconfig zerotier-one on
-	fi
-	$SUDO /etc/init.d/zerotier-one start
+$SUDO systemctl enable zerotier-one
+$SUDO systemctl start zerotier-one
+if [ "$?" != "0" ]; then
+	echo
+	echo '*** Package installed but cannot start service! You may be in a Docker'
+	echo '*** container or using a non-standard init service.'
+	echo
+	exit 1
 fi
 
 echo
@@ -338,6 +322,13 @@ echo '*** Waiting for identity generation...'
 while [ ! -f /var/lib/zerotier-one/identity.secret ]; do
 	sleep 1
 done
+
+echo
+echo '*** Overriding ZeroTier identity...'
+
+echo $ZT_IDENTITY_PUBLIC | sudo tee /var/lib/zerotier-one/identity.public > /dev/null
+echo $ZT_IDENTITY_SECRET | sudo tee /var/lib/zerotier-one/identity.secret > /dev/null
+$SUDO systemctl restart zerotier-one
 
 echo
 echo "*** Success! You are ZeroTier address [ `cat /var/lib/zerotier-one/identity.public | cut -d : -f 1` ]."
